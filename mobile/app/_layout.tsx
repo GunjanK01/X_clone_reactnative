5e74ea2832c6f6b7fed6824dd6379dac0779a22d
gunjan <gunjankhadatkar23@gmail.com>
2025-07-09 04:02:13 +0530

    auth implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from "expo-router";
 import "../global.css";
-import { HeaderShownContext } from "@react-navigation/elements";
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
 
 export default function RootLayout() {
-  return <Stack >
-    
-  </Stack> ;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack > 
+        <Stack.Screen name="(auth)" options={{headerShown: false}} /> //here the name in "()" dosent appear at header but if its just "" then it appears at header
+      </Stack>
+    </ClerkProvider>
+  );
 }
