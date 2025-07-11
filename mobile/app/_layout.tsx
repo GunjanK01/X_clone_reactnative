cdc729f0146bb962f2d2b422ca6c43937e4443c7
gunjan <gunjankhadatkar23@gmail.com>
2025-07-12 02:31:43 +0530

    signout button and tabs setup done

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -7,7 +7,7 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache'
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack > 
+      <Stack  screenOptions={{ headerShown: false }}> 
         <Stack.Screen name="(auth)" options={{headerShown: false}} /> //here the name in "()" dosent appear at header but if its just "" then it appears at header
       </Stack>
     </ClerkProvider>
