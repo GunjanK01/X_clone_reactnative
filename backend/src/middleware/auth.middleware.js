900c43ff7f81672d5dbcf0d09bfefee664117f05
gunjan <gunjankhadatkar23@gmail.com>
2025-07-18 01:52:11 +0530

    changes back

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,12 +1,5 @@
-// export const protectRoute = async (req, res, next) => {
-//   if (!req.auth().isAuthenticated) {
-//     return res.status(401).json({ message: "Unauthorized - you must be logged in" });
-//   }
-//   next();
-// };
-
-export const protectRoute = (req, res, next) => {
-  if (!req.auth || !req.auth.userId) {
+export const protectRoute = async (req, res, next) => {
+  if (!req.auth().isAuthenticated) {
     return res.status(401).json({ message: "Unauthorized - you must be logged in" });
   }
   next();
