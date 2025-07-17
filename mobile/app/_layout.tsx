09c94c139287050feb809305e4e20309a4fc33c4
gunjan <gunjankhadatkar23@gmail.com>
2025-07-18 01:49:58 +0530

    some changes

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,14 +2,17 @@ import { Stack } from "expo-router";
 import "../global.css";
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
+const queryClient = new QueryClient();
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack  screenOptions={{ headerShown: false }}> 
-        <Stack.Screen name="(auth)" options={{headerShown: false}} /> //here the name in "()" dosent appear at header but if its just "" then it appears at header
-      </Stack>
+      <QueryClientProvider client={queryClient}>
+        <Stack  screenOptions={{ headerShown: false }}> 
+          <Stack.Screen name="(auth)" options={{headerShown: false}} /> //here the name in "()" dosent appear at header but if its just "" then it appears at header
+        </Stack>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
